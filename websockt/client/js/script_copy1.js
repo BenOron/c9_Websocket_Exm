ca4728475a6a8da7d0716ad0e356d984df958909
benoron <ben.oron1@gmail.com>
2016-02-16 11:04:12 +0000

    new copy

---
+++ websockt/client/js/script_copy1.js
@@ -0,0 +1,74 @@
+var socket = io.connect();
+
+
+$('#connect').on('click', function(e){
+    e.preventDefault();
+    var options = {
+      otherUserID: 2  //the userID of the user you are trying to contact.
+    };
+    
+    socket.emit('connect_to_other', options);
+        socket.emit('chat message', "runninig...");
+
+    // return false;
+});
+
+$('#chat').on('click', function(e){
+    e.preventDefault();
+    var msg = $('#input1').val();
+     socket.emit('chat message', msg);
+    $('#input1').val('');
+});
+
+socket.on('connect', function(){
+    console.log('user connected client side');
+    
+    socket.on('open chat window',function(success){
+        if(success){
+            $('#chat_window').removeClass('none').prepend('<p>Chat with userID ' + success.otherUserID + ':');
+        }
+    });
+    
+    socket.on('chat message', function(msg){
+        $('#messages').append($('<li>').text('User ' + msg.userID + ': ' + msg.msg)); 
+    });
+    
+    socket.on('call to join', function(msg){
+      socket.emit('join request user room', msg);    
+    });
+});
+
+
+//reset
+// var userID = Math.ceil(Math.random()*10000);
+
+// var socket = io.connect();
+    
+// $('form').submit(function(){
+    
+//     var options = {
+//       input: $('#input1').val(),
+//       userID: userID
+//     };
+    
+//     // socket.emit('log in', options);
+    
+//     socket.emit('chat message', options);
+//     $('#input1').val('');
+//     return false;
+// });
+
+// socket.on('connect', function(){
+//     console.log('user connected client side');
+    
+//     socket.on('chat message', function(msg){
+//         $('#messages').append($('<li>').text(msg.input)); 
+//     });
+// });
+
+
+$('#remove_socket').on('click', function(){
+   socket.disconnect();
+});
+  
+    
